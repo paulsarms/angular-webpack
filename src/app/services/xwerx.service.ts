cd906f04b1e89959dd495dc035ea0af31341e79e
paulsarms <armstrong.pa@gmail.com>
2017-10-05 16:06:58 +0100

    adding data service

---
+++ src/app/services/xwerx.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class XwerxService {
+  private _dataUrl: string = "https://raw.githubusercontent.com/paulsarms/angular-webpack/master/src/data/sample_data.json";
+  //private _dataUrl: string = "data/sample_data.json";
+  constructor (private _http: Http) {  }
+
+  getData(): Observable<any> {
+      return this._http.get(this._dataUrl)
+      .map((response: Response) => <any>response.json());
+    }
+}
