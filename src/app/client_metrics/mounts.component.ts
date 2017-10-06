986fcb4d641f4054670ad88e3f4719c987e3f8a0
paulsarms <armstrong.pa@gmail.com>
2017-10-06 08:28:27 +0100

    adding mounts component

---
+++ src/app/client_metrics/mounts.component.ts
@@ -0,0 +1,60 @@
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { XwerxService } from '../services/xwerx.service';
+
+@Component({
+  selector: 'mounts-component',
+  templateUrl: './mounts.component.html',
+  styleUrls: ['./mounts.component.scss']
+})
+
+export class MountsComponent implements OnInit  {
+  title:string = "Last 12 Mounts";
+  blue: string = "#69A3E5";
+  grey: string = "#596169";
+  data: any;
+  canvas: any;
+  errorMessage: string;
+  @ViewChild('mountscanvas') canvasRef:ElementRef;
+
+
+  constructor(private _xwerxService: XwerxService) { }
+
+  ngOnInit() {
+      this.getData();
+      this.canvas = this.canvasRef.nativeElement;
+    }
+
+  getData() {
+    this._xwerxService.getData().subscribe(
+      resData => {
+        this.data = resData;
+        this.draw(resData);
+      },
+      error => this.errorMessage = <any>error);
+  }
+  draw(data: any) {
+      const ctx = this.canvas.getContext('2d');
+      let W = this.canvas.width;
+      let H = this.canvas.height;
+      let barwidth = W/24;
+      let barspace = W/12;
+      let color = this.blue
+      let bgcolor = this.grey;
+      // Clear the canvas everytime a chart is drawn:
+      ctx.clearRect(0, 0, barwidth, H);
+
+
+      for(let i=0;i<22;i++){
+        let x = i*barspace;
+        ctx.fillStyle = bgcolor;
+        ctx.fillRect(x,0,barwidth,H);
+      }
+      let mounts = data.clients.mounts;
+      for (let i in mounts) {
+        let x = parseInt(i)*barspace;
+        let y = H * mounts[i] * 0.1;
+        ctx.fillStyle = color;
+        ctx.fillRect(x,H-y,barwidth,y);
+      }
+    }
+  }
